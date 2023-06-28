83ab3ee193112c666dcd9a16ca26ec4b8282317d
HubCreator <chocomilk8604@gmail.com>
2023-06-28 19:07:26 +0900

    feat: FieldServiceTest 작성

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,35 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> fieldService.logic("useA");
+        Runnable userB = () -> fieldService.logic("useB");
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(2000);
+        threadB.start();
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
